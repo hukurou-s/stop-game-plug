98c48829bc209a8e9c25684178ecc740cb698482
Reo ISHII <hukurou@mercari.com>
2020-12-05 17:48:26 +0900

    add Dockerfile and implement feature to get device info

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client } = require('tplink-smarthome-api');
+
+const env = process.env;
+const client = new Client();
+
+client.getDevice({ host: env.DEVICE_IP }).then((device) => {
+    device.getSysInfo().then(console.log);
+});
