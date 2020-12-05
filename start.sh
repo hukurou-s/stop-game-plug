39050cad216d189f81f73e233b5dd3bba3aaf26d
Reo ISHII <hukurou@mercari.com>
2020-12-05 20:43:24 +0900

    move start script file

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-docker build -t cron .
+docker build -t docker build -t stop-game-plug .
+docker build -t cron ./cron/
 docker run -v /var/run/docker.sock:/var/run/docker.sock -e DEVICE_IP=$1 cron
