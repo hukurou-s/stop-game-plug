379128cf197c1fefe865f2dcc4fdcb661725dfe6
Reo ISHII <hukurou@mercari.com>
2020-12-07 00:41:45 +0900

    implement slack notification

--- cron/cmd/prevent_breaking_start_cmd.sh
+++ cron/cmd/prevent_breaking_start_cmd.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker run -d -e DEVICE_IP=$1 --name prevent-breaking-container --rm prevent-breaking
+docker run -d -e DEVICE_IP=$1 -e SLACK_WEBHOOK_URL=$2 --name prevent-breaking-container --rm prevent-breaking
--- prevent-breaking/index.js
+++ prevent-breaking/index.js
@@ -1,14 +1,23 @@
 const { Client } = require('tplink-smarthome-api');
+const { IncomingWebhook } = require('@slack/webhook');
 
 const env = process.env;
+const url = process.env.SLACK_WEBHOOK_URL;
+
 const client = new Client({
     defaultSendOptions: { timeout: 20000, transport: 'tcp' },
 });
+const webhook = new IncomingWebhook(url);
 
 const monitorEvents = function monitorEvents(device) {
     device.on('in-use', () => {
         device.setPowerState(false);
         console.log("turn off");
+        (async () => {
+            await webhook.send({
+                text: '起動されたので、電源を切りました。',
+            });
+        })();
     });
 
     device.startPolling(5000);
