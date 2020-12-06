00cf162e142831b49da9e39f040cd8eb8a137636
Reo ISHII <hukurou@mercari.com>
2020-12-06 16:11:02 +0900

    move to new directory

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-docker build -t docker build -t stop-game-plug .
+docker build -t docker build -t stop-game-plug ./stop/
 docker build -t cron ./cron/
 docker run -v /var/run/docker.sock:/var/run/docker.sock -e DEVICE_IP=$1 cron
